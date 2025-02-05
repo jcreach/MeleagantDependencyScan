01e40e3e2cbe470e9f823a2a608851ae0b77d69f
Julien Creach <julien.creach@pm.me>
2025-02-05 01:07:14 +0100

    add keyed services

--- Src/MeleagantDependencyScan.UnitTest/MeleagantServiceCollectionExtensionsTests.cs
+++ Src/MeleagantDependencyScan.UnitTest/MeleagantServiceCollectionExtensionsTests.cs
@@ -1,3 +1,4 @@
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Reflection;
 using FluentAssertions;
@@ -9,6 +10,8 @@ namespace MeleagantDependencyScan.UnitTest
 {
     public class MeleagantServiceCollectionExtensionsTests
     {
+        #region Simple services
+
         [Fact]
         public void ServiceCollection_Should_Contain_Transient_Visible_By_ItSelf_HelloWorldService()
         {
@@ -128,5 +131,139 @@ namespace MeleagantDependencyScan.UnitTest
 
             testResult.Should().NotBeNullOrEmpty().And.HaveCount(1);
         }
+
+        #endregion
+
+        #region Keyed services
+
+        [Fact]
+        public void ServiceCollection_Should_Contain_Keyed_Transient_Visible_By_Interface_HelloWorldKeyedService()
+        {
+            // Arrange
+
+            var sc = new ServiceCollection();
+            sc.Clear();
+            sc.ScanKeyedAssemblies(Assembly.GetExecutingAssembly().GetName().Name!);
+
+            // Act
+
+            var testResult = sc.Where(s => s.ServiceType == typeof(IHelloWorldKeyedServices)
+                                           && s.IsKeyedService
+                                           && s.ServiceKey!.ToString() == "TKeyA"
+                                            && s.KeyedImplementationType == typeof(HelloWorldKeyedTransientServicesWithInterfaceKeyA) 
+                                            && s.Lifetime == ServiceLifetime.Transient);
+            
+            // Assert
+            testResult.Should().NotBeNullOrEmpty().And.HaveCount(1);
+        }
+        
+        [Theory]
+        [InlineData("TKeyA")]
+        [InlineData("TKeyB")]
+        public void ServiceProvider_Should_Provide_TKey_Transient_Visible_By_Interface_HelloWorldKeyedService(string key)
+        {
+            // Arrange
+
+            var sc = new ServiceCollection();
+            sc.Clear();
+            sc.ScanKeyedAssemblies(Assembly.GetExecutingAssembly().GetName().Name!);
+            var sp = sc.BuildServiceProvider();
+            
+            // Act
+
+            var testResult = sp.GetKeyedService<IHelloWorldKeyedServices>(key)?.SayHiKeyed($"{key} service");
+ 
+            // Assert
+            testResult.Should().Be($"Hello Keyed World {key} service with interface");
+        }
+        
+        [Fact]
+        public void ServiceCollection_Should_Contain_Keyed_Scoped_Visible_By_Interface_HelloWorldKeyedService()
+        {
+            // Arrange
+
+            var sc = new ServiceCollection();
+            sc.Clear();
+            sc.ScanKeyedAssemblies(Assembly.GetExecutingAssembly().GetName().Name!);
+
+            // Act
+
+            var testResult = sc.Where(s => s.ServiceType == typeof(IHelloWorldKeyedServices)
+                                           && s.IsKeyedService
+                                           && s.ServiceKey!.ToString() == "SKeyA"
+                                           && s.KeyedImplementationType == typeof(HelloWorldKeyedScopedServicesWithInterfaceKeyA) 
+                                           && s.Lifetime == ServiceLifetime.Scoped);
+            
+            // Assert
+            
+            testResult.Should().NotBeNullOrEmpty().And.HaveCount(1);
+        }
+        
+        [Theory]
+        [InlineData("SKeyA")]
+        [InlineData("SKeyB")]
+        public void ServiceProvider_Should_Provide_SKey_Scoped_Visible_By_Interface_HelloWorldKeyedService(string key)
+        {
+            // Arrange
+
+            var sc = new ServiceCollection();
+            sc.Clear();
+            sc.ScanKeyedAssemblies(Assembly.GetExecutingAssembly().GetName().Name!);
+            var sp = sc.BuildServiceProvider();
+            
+            // Act
+
+            var testResult = sp.GetKeyedService<IHelloWorldKeyedServices>(key)?.SayHiKeyed($"{key} service");
+ 
+            // Assert
+            
+            testResult.Should().Be($"Hello Keyed World {key} service with interface");
+        }
+        
+        [Fact]
+        public void ServiceCollection_Should_Contain_Keyed_Singleton_Visible_By_Interface_HelloWorldKeyedService()
+        {
+            // Arrange
+
+            var sc = new ServiceCollection();
+            sc.Clear();
+            sc.ScanKeyedAssemblies(Assembly.GetExecutingAssembly().GetName().Name!);
+
+            // Act
+
+            var testResult = sc.Where(s => s.ServiceType == typeof(IHelloWorldKeyedServices)
+                                           && s.IsKeyedService
+                                           && s.ServiceKey!.ToString() == "SiKeyA"
+                                           && s.KeyedImplementationType == typeof(HelloWorldKeyedSingletonServicesWithInterfaceKeyA) 
+                                           && s.Lifetime == ServiceLifetime.Singleton);
+            
+            // Assert
+            
+            testResult.Should().NotBeNullOrEmpty().And.HaveCount(1);
+        }
+        
+        [Theory]
+        [InlineData("SiKeyA")]
+        [InlineData("SiKeyB")]
+        public void ServiceProvider_Should_Provide_SiKey_Singleton_Visible_By_Interface_HelloWorldKeyedService(string key)
+        {
+            // Arrange
+
+            var sc = new ServiceCollection();
+            sc.Clear();
+            sc.ScanKeyedAssemblies(Assembly.GetExecutingAssembly().GetName().Name!);
+            var sp = sc.BuildServiceProvider();
+            
+            // Act
+
+            var testResult = sp.GetKeyedService<IHelloWorldKeyedServices>(key)?.SayHiKeyed($"{key} service");
+ 
+            // Assert
+            
+            testResult.Should().Be($"Hello Keyed World {key} service with interface");
+        }
+
+        #endregion
+
     }
 }
--- Src/MeleagantDependencyScan/Extensions/MeleagantServiceCollectionExtensions.cs
+++ Src/MeleagantDependencyScan/Extensions/MeleagantServiceCollectionExtensions.cs
@@ -17,8 +17,7 @@ namespace MeleagantDependencyScan.Extensions
             IDictionary<Type, MeleagantInjectionAttribute?> toInject = assemblies
                 .SelectMany(a => a.GetTypes())
                 .Where(t => Attribute.IsDefined(t, typeof(MeleagantInjectionAttribute))).ToDictionary(t => t, t => t.GetCustomAttribute<MeleagantInjectionAttribute>());
-
-
+            
             foreach ((Type implType, MeleagantInjectionAttribute? metadatas) in toInject)
             {
                 if(metadatas is null)
@@ -60,6 +59,59 @@ namespace MeleagantDependencyScan.Extensions
                 }
             }
 
+            return serviceCollection;
+        }
+
+        public static IServiceCollection ScanKeyedAssemblies(this IServiceCollection serviceCollection,
+            params string[] assembliesNames)
+        {
+            IEnumerable<Assembly> assemblies = assembliesNames.Select(Assembly.Load);
+
+            IDictionary<Type, MeleagantInjectionKeyedAttribute?> toInject = assemblies
+                .SelectMany(a => a.GetTypes())
+                .Where(t => Attribute.IsDefined(t, typeof(MeleagantInjectionKeyedAttribute))).ToDictionary(t => t, t => t.GetCustomAttribute<MeleagantInjectionKeyedAttribute>());
+
+            foreach ((Type implType, MeleagantInjectionKeyedAttribute? metadatas) in toInject)
+            {
+                if(metadatas is null || string.IsNullOrWhiteSpace(metadatas.Key))
+                    continue;
+                
+                if (metadatas.VisibleFromInterface)
+                {
+                    if (metadatas.VisibleAs is null || !metadatas.VisibleAs.Any())
+                    {
+                        throw new MeleagantInjectionNotVisibleByItSelfException();
+                    }
+                    else
+                    {
+                        Type firstInterfaceType = metadatas.VisibleAs.First();
+                        serviceCollection.Add(new ServiceDescriptor(firstInterfaceType, metadatas.Key, implType, metadatas.LifeTime));
+
+                        // This ensures that if it is registered via 1 or more interfaces, the instance will always be the same.
+                        if (metadatas.VisibleAs.Length <= 1)
+                            continue;
+
+                        foreach (Type otherInterfaceType in metadatas.VisibleAs.Skip(1))
+                        {
+                            serviceCollection.Add(new ServiceDescriptor(otherInterfaceType, sp => sp.GetService(firstInterfaceType)!, metadatas.LifeTime));
+                        }
+                    }
+                }
+                else // Visible by itself
+                {
+                    serviceCollection.Add(new ServiceDescriptor(implType, metadatas.Key, implType, metadatas.LifeTime));
+
+                    // This ensures that if it is registered via 1 or more interfaces, the instance will always be the same.
+                    if (metadatas.VisibleAs is null)
+                        continue;
+
+                    foreach (Type interfaceType in metadatas.VisibleAs)
+                    {
+                        serviceCollection.Add(new ServiceDescriptor(interfaceType, sp => sp.GetService(implType)!, metadatas.LifeTime));
+                    }
+                }
+            }
+            
             return serviceCollection;
         }
     }
