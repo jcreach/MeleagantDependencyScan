ea9a909749dce8cdf0719ac46643993c4d1a69c4
Julien Creach <julien.creach@cgi.com>
2026-08-28 17:23:49 +0200

    draft

--- Src/MeleagantDependencyScan.UnitTest/MeleagantServiceCollectionExtensionsTests.cs
+++ Src/MeleagantDependencyScan.UnitTest/MeleagantServiceCollectionExtensionsTests.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Linq;
 using System.Reflection;
 using MeleagantDependencyScan.Extensions;
 using MeleagantDependencyScan.UnitTest.TestServices;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace MeleagantDependencyScan.UnitTest
 {
@@ -130,6 +132,84 @@ namespace MeleagantDependencyScan.UnitTest
             Assert.Single(testResult);
         }
 
+        [Fact]
+        public void ServiceProvider_Should_Resolve_Closed_Generic_Service_From_Open_Generic_Registration()
+        {
+            var sc = new ServiceCollection();
+            sc.ScanAssemblies(Assembly.GetExecutingAssembly().GetName().Name!);
+            var sp = sc.BuildServiceProvider();
+
+            var executor = sp.GetServices<IMockGenericInterface<MockRequest, MockResponse>>()
+                .Single(service => service.GetType() == typeof(MockGenericClass<MockRequest, MockResponse>));
+
+            Assert.Equal("Executed MeleagantDependencyScan.UnitTest.TestServices.MockRequest", executor.Execute(new MockRequest()));
+        }
+
+        [Theory]
+        [InlineData(typeof(MockGenericClass<,>), ServiceLifetime.Scoped)]
+        [InlineData(typeof(MockGenericTransientClass<,>), ServiceLifetime.Transient)]
+        [InlineData(typeof(MockGenericSingletonClass<,>), ServiceLifetime.Singleton)]
+        public void ServiceCollection_Should_Register_Open_Generic_Service_With_Expected_Lifetime(Type implementationType, ServiceLifetime lifetime)
+        {
+            var sc = new ServiceCollection();
+
+            sc.ScanAssemblies(Assembly.GetExecutingAssembly().GetName().Name!);
+
+            var registration = sc.Single(service =>
+                service.ServiceType == typeof(IMockGenericInterface<,>) &&
+                service.ImplementationType == implementationType);
+
+            Assert.Equal(lifetime, registration.Lifetime);
+        }
+
+        [Fact]
+        public void ServiceCollection_Should_Throw_When_Exact_NonKeyed_Registration_Is_Duplicated()
+        {
+            // Arrange
+            var sc = new ServiceCollection();
+            sc.Add(new ServiceDescriptor(typeof(IHelloWorldService), typeof(HelloWorldTransientServicesWithInterface), ServiceLifetime.Transient));
+
+            // Act
+            var act = () => MeleagantServiceCollectionExtensions.ValidateNoDuplicateRegistration(sc, typeof(IHelloWorldService), typeof(HelloWorldTransientServicesWithInterface), key: null);
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(act);
+        }
+
+        [Fact]
+        public void ServiceCollection_Should_Throw_When_Exact_Keyed_Registration_Is_Duplicated()
+        {
+            // Arrange
+            var sc = new ServiceCollection();
+            sc.Add(new ServiceDescriptor(typeof(IHelloWorldKeyedServices), "DuplicateKey", typeof(HelloWorldKeyedTransientServicesWithInterfaceKeyA), ServiceLifetime.Transient));
+
+            // Act
+            var act = () => MeleagantServiceCollectionExtensions.ValidateNoDuplicateRegistration(sc, typeof(IHelloWorldKeyedServices), typeof(HelloWorldKeyedTransientServicesWithInterfaceKeyA), key: "DuplicateKey");
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(act);
+        }
+
+        [Fact]
+        public void ScanAssemblies_Should_Throw_When_Assembly_Name_Is_Empty()
+        {
+            var sc = new ServiceCollection();
+
+            var act = () => sc.ScanAssemblies(string.Empty);
+
+            Assert.Throws<ArgumentException>(act);
+        }
+
+        [Fact]
+        public void ScanKeyedAssemblies_Should_Throw_When_Assembly_Names_Are_Missing()
+        {
+            var sc = new ServiceCollection();
+
+            var act = () => sc.ScanKeyedAssemblies();
+
+            Assert.Throws<ArgumentException>(act);
+        }
+
         #endregion
 
         #region Keyed services
@@ -173,6 +253,30 @@ namespace MeleagantDependencyScan.UnitTest
 
             // Assert
             Assert.Equal($"Hello Keyed World {key} service with interface", testResult);
+        }
+
+        [Fact]
+        public void ServiceProvider_Should_Provide_Keyed_MultiInterface_Service()
+        {
+            var sc = new ServiceCollection();
+            sc.ScanKeyedAssemblies(Assembly.GetExecutingAssembly().GetName().Name!);
+            var sp = sc.BuildServiceProvider();
+
+            var service = sp.GetKeyedService<IHelloWorldKeyedSecondaryServices>("MultiKey");
+
+            Assert.Equal("Hello Keyed Secondary World secondary service with interface", service?.SayHiKeyedSecondary("secondary service"));
+        }
+
+        [Fact]
+        public void ServiceProvider_Should_Resolve_Keyed_Closed_Generic_Service_From_Open_Generic_Registration()
+        {
+            var sc = new ServiceCollection();
+            sc.ScanKeyedAssemblies(Assembly.GetExecutingAssembly().GetName().Name!);
+            var sp = sc.BuildServiceProvider();
+
+            var service = sp.GetKeyedService<IKeyedMockGenericInterface<MockRequest, MockResponse>>("GenericKey");
+
+            Assert.Equal("Executed keyed MeleagantDependencyScan.UnitTest.TestServices.MockRequest", service?.Execute(new MockRequest()));
         }
 
         [Fact]
--- Src/MeleagantDependencyScan/Extensions/MeleagantServiceCollectionExtensions.cs
+++ Src/MeleagantDependencyScan/Extensions/MeleagantServiceCollectionExtensions.cs
@@ -10,8 +10,68 @@ namespace MeleagantDependencyScan.Extensions
 {
     public static class MeleagantServiceCollectionExtensions
     {
+        public static void ValidateVisibleAs(Type implType, Type[]? visibleAs)
+        {
+            if (visibleAs is null || !visibleAs.Any())
+            {
+                throw new MeleagantInjectionNotVisibleByItSelfException();
+            }
+
+            foreach (Type interfaceType in visibleAs)
+            {
+                if (!interfaceType.IsInterface)
+                {
+                    throw new InvalidOperationException($"The type '{interfaceType.Name}' in VisibleAs must be an interface. '{implType.Name}' is not valid for interface-based registration.");
+                }
+
+                bool isAssignable = interfaceType.IsAssignableFrom(implType);
+                if (!isAssignable && interfaceType.IsGenericTypeDefinition && implType.IsGenericTypeDefinition)
+                {
+                    isAssignable = implType.GetInterfaces()
+                        .Any(implementedInterface => implementedInterface.IsGenericType
+                            && implementedInterface.GetGenericTypeDefinition() == interfaceType);
+                }
+
+                if (!isAssignable)
+                {
+                    throw new InvalidOperationException($"The type '{implType.Name}' does not implement the interface '{interfaceType.Name}' declared in VisibleAs.");
+                }
+            }
+        }
+
+        public static void ValidateNoDuplicateRegistration(this IServiceCollection serviceCollection, Type serviceType, Type implementationType, object? key = null)
+        {
+            bool isDuplicate = serviceCollection.Any(descriptor =>
+                descriptor.ServiceType == serviceType &&
+                ((descriptor.ImplementationType == implementationType) ||
+                 (descriptor.IsKeyedService && descriptor.KeyedImplementationType == implementationType)) &&
+                (key is null
+                    ? !descriptor.IsKeyedService
+                    : descriptor.IsKeyedService && Equals(descriptor.ServiceKey, key)));
+
+            if (isDuplicate)
+            {
+                var keyDescription = key is null ? "non-keyed" : $"keyed '{key}'";
+                throw new InvalidOperationException($"A duplicate registration already exists for service '{serviceType.Name}' ({keyDescription}) with implementation '{implementationType.Name}'.");
+            }
+        }
+
+        private static void ValidateAssemblyNames(string[]? assembliesNames)
+        {
+            if (assembliesNames is null || assembliesNames.Length == 0)
+            {
+                throw new ArgumentException("At least one assembly name must be provided.", nameof(assembliesNames));
+            }
+
+            if (assembliesNames.Any(string.IsNullOrWhiteSpace))
+            {
+                throw new ArgumentException("Assembly names cannot be null, empty, or whitespace.", nameof(assembliesNames));
+            }
+        }
+
         public static IServiceCollection ScanAssemblies(this IServiceCollection serviceCollection, params string[] assembliesNames)
         {
+            ValidateAssemblyNames(assembliesNames);
             IEnumerable<Assembly> assemblies = assembliesNames.Select(Assembly.Load);
 
             IDictionary<Type, MeleagantInjectionAttribute?> toInject = assemblies
@@ -23,37 +83,40 @@ namespace MeleagantDependencyScan.Extensions
                 if(metadatas is null)
                     continue;
 
+                Type[]? visibleAs = metadatas.VisibleAs;
+
                 if (metadatas.VisibleFromInterface)
                 {
-                    if (metadatas.VisibleAs is null || !metadatas.VisibleAs.Any())
-                    {
-                        throw new MeleagantInjectionNotVisibleByItSelfException();
-                    }
-                    else
-                    {
-                        Type firstInterfaceType = metadatas.VisibleAs.First();
-                        serviceCollection.Add(new ServiceDescriptor(firstInterfaceType, implType, metadatas.LifeTime));
+                    Type[] validatedVisibleAs = visibleAs ?? throw new MeleagantInjectionNotVisibleByItSelfException();
+                    ValidateVisibleAs(implType, validatedVisibleAs);
 
-                        // This ensures that if it is registered via 1 or more interfaces, the instance will always be the same.
-                        if (metadatas.VisibleAs.Length <= 1)
-                            continue;
+                    Type firstInterfaceType = validatedVisibleAs[0];
+                    serviceCollection.ValidateNoDuplicateRegistration(firstInterfaceType, implType);
+                    serviceCollection.Add(new ServiceDescriptor(firstInterfaceType, implType, metadatas.LifeTime));
 
-                        foreach (Type otherInterfaceType in metadatas.VisibleAs.Skip(1))
-                        {
-                            serviceCollection.Add(new ServiceDescriptor(otherInterfaceType, sp => sp.GetService(firstInterfaceType)!, metadatas.LifeTime));
-                        }
+                    // This ensures that if it is registered via 1 or more interfaces, the instance will always be the same.
+                    if (validatedVisibleAs.Length <= 1)
+                        continue;
+
+                    foreach (Type otherInterfaceType in validatedVisibleAs.Skip(1))
+                    {
+                        serviceCollection.ValidateNoDuplicateRegistration(otherInterfaceType, implType);
+                        serviceCollection.Add(new ServiceDescriptor(otherInterfaceType, sp => sp.GetService(firstInterfaceType)!, metadatas.LifeTime));
                     }
                 }
                 else // Visible by itself
                 {
+                    serviceCollection.ValidateNoDuplicateRegistration(implType, implType);
                     serviceCollection.Add(new ServiceDescriptor(implType, implType, metadatas.LifeTime));
 
                     // This ensures that if it is registered via 1 or more interfaces, the instance will always be the same.
-                    if (metadatas.VisibleAs is null) 
+                    if (visibleAs is null) 
                         continue;
 
-                    foreach (Type interfaceType in metadatas.VisibleAs)
+                    ValidateVisibleAs(implType, visibleAs);
+                    foreach (Type interfaceType in visibleAs)
                     {
+                        serviceCollection.ValidateNoDuplicateRegistration(interfaceType, implType);
                         serviceCollection.Add(new ServiceDescriptor(interfaceType, sp => sp.GetService(implType)!, metadatas.LifeTime));
                     }
                 }
@@ -65,6 +128,7 @@ namespace MeleagantDependencyScan.Extensions
         public static IServiceCollection ScanKeyedAssemblies(this IServiceCollection serviceCollection,
             params string[] assembliesNames)
         {
+            ValidateAssemblyNames(assembliesNames);
             IEnumerable<Assembly> assemblies = assembliesNames.Select(Assembly.Load);
 
             IDictionary<Type, MeleagantInjectionKeyedAttribute?> toInject = assemblies
@@ -75,39 +139,50 @@ namespace MeleagantDependencyScan.Extensions
             {
                 if(metadatas is null || string.IsNullOrWhiteSpace(metadatas.Key))
                     continue;
+
+                Type[]? visibleAs = metadatas.VisibleAs;
                 
                 if (metadatas.VisibleFromInterface)
                 {
-                    if (metadatas.VisibleAs is null || !metadatas.VisibleAs.Any())
-                    {
-                        throw new MeleagantInjectionNotVisibleByItSelfException();
-                    }
-                    else
-                    {
-                        Type firstInterfaceType = metadatas.VisibleAs.First();
-                        serviceCollection.Add(new ServiceDescriptor(firstInterfaceType, metadatas.Key, implType, metadatas.LifeTime));
+                    Type[] validatedVisibleAs = visibleAs ?? throw new MeleagantInjectionNotVisibleByItSelfException();
+                    ValidateVisibleAs(implType, validatedVisibleAs);
 
-                        // This ensures that if it is registered via 1 or more interfaces, the instance will always be the same.
-                        if (metadatas.VisibleAs.Length <= 1)
-                            continue;
+                    Type firstInterfaceType = validatedVisibleAs[0];
+                    serviceCollection.ValidateNoDuplicateRegistration(firstInterfaceType, implType, metadatas.Key);
+                    serviceCollection.Add(new ServiceDescriptor(firstInterfaceType, metadatas.Key, implType, metadatas.LifeTime));
 
-                        foreach (Type otherInterfaceType in metadatas.VisibleAs.Skip(1))
-                        {
-                            serviceCollection.Add(new ServiceDescriptor(otherInterfaceType, sp => sp.GetService(firstInterfaceType)!, metadatas.LifeTime));
-                        }
+                    // This ensures that if it is registered via 1 or more interfaces, the instance will always be the same.
+                    if (validatedVisibleAs.Length <= 1)
+                        continue;
+
+                    foreach (Type otherInterfaceType in validatedVisibleAs.Skip(1))
+                    {
+                        serviceCollection.ValidateNoDuplicateRegistration(otherInterfaceType, implType, metadatas.Key);
+                        serviceCollection.Add(new ServiceDescriptor(
+                            otherInterfaceType,
+                            metadatas.Key,
+                            (sp, serviceKey) => sp.GetKeyedService(firstInterfaceType, serviceKey)!,
+                            metadatas.LifeTime));
                     }
                 }
                 else // Visible by itself
                 {
+                    serviceCollection.ValidateNoDuplicateRegistration(implType, implType, metadatas.Key);
                     serviceCollection.Add(new ServiceDescriptor(implType, metadatas.Key, implType, metadatas.LifeTime));
 
                     // This ensures that if it is registered via 1 or more interfaces, the instance will always be the same.
-                    if (metadatas.VisibleAs is null)
+                    if (visibleAs is null)
                         continue;
 
-                    foreach (Type interfaceType in metadatas.VisibleAs)
+                    ValidateVisibleAs(implType, visibleAs);
+                    foreach (Type interfaceType in visibleAs)
                     {
-                        serviceCollection.Add(new ServiceDescriptor(interfaceType, sp => sp.GetService(implType)!, metadatas.LifeTime));
+                        serviceCollection.ValidateNoDuplicateRegistration(interfaceType, implType, metadatas.Key);
+                        serviceCollection.Add(new ServiceDescriptor(
+                            interfaceType,
+                            metadatas.Key,
+                            (sp, serviceKey) => sp.GetKeyedService(implType, serviceKey)!,
+                            metadatas.LifeTime));
                     }
                 }
             }
